3f7a8b074aaf9d12c727d918c496bf609e4edcad
theaman82 <amankumaraman55555@gmail.com>
2025-07-12 09:28:13 +0530

    restructure files

--- resources/views/livewire/admin/offers/show-all.blade.php
+++
@@ -1,6 +0,0 @@
-<div>
-    <button wire:click="$dispatch('open-create-modal')" class="font-semibold text-lg bg-red-500 px-3 py-2 rounded">Add
-        New Offer</a>
-        <!-- livewire modals -->
-        <livewire:admin.offers.add />
-</div>
