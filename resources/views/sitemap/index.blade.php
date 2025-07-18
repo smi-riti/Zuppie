4414ae2cb7ba38b767efbd95b173d7fcf6e665a6
smi-riti <smriti.kumari5104@gmail.com>
2025-07-18 17:46:48 +0530

    Updates sitemap views to use object property access
    Switches from array to object property access for sitemap and page data in views, ensuring compatibility with data passed as objects. Improves template consistency and reduces potential access errors.

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -2,8 +2,8 @@
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 @foreach($sitemaps as $sitemap)
     <sitemap>
-        <loc>{{ $sitemap['loc'] }}</loc>
-        <lastmod>{{ $sitemap['lastmod'] }}</lastmod>
+        <loc>{{ $sitemap->loc }}</loc>
+        <lastmod>{{ $sitemap->lastmod }}</lastmod>
     </sitemap>
 @endforeach
 </sitemapindex>
