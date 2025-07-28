9ec163ef588aecce2131c6b7662fbd8987214de1
puja-03 <pujakumari11th2003@gmail.com>
2025-07-28 16:53:05 +0530

    email configration

--- app/Mail/LoginNotificationMail.php
+++ app/Mail/LoginNotificationMail.php
@@ -11,52 +11,24 @@ class LoginNotificationMail extends Mailable
     use Queueable, SerializesModels;
 
     public $user;
-    public $subject;
-    public $msg;
 
     /**
      * Create a new message instance.
      */
-    public function __construct($msg, $subject)
+    public function __construct($user)
     {
-        // $this->user = $user;
-        $this->msg = $msg;
-        $this->subject = $subject;
+        $this->user = $user;
     }
 
     /**
      * Get the message envelope.
      */
-    public function envelope(): Envelope
-    {
-        return new Envelope(
-            subject: $this->subject,
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
-    }
+    // ...existing code...
 
     public function build()
     {
         return $this->subject('Login Notification')
-            ->view('emails.login-notification');
+            ->view('emails.login-notification')
+            ->with(['user' => $this->user]);
     }
 }
---
+++ resources/views/emails/login-notification.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    {{-- <p>Hello {{ $user->name }},</p> --}}
+    <p>You have successfully logged in to your account cvbnm,fghjkl dfghjvbn    </p>
+    <p>If this wasn't you, please contact support immediately.</p>
+</body>
+</html>
