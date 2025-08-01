6912f78828396d679af0088e2dd16b50e6010f7a
smi-riti <smriti.kumari5104@gmail.com>
2025-08-01 14:41:20 +0530

    css fixed

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -80,7 +80,7 @@
     <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@400;500;600;700;800&display=swap"></noscript>
     
     <!-- Tailwind CSS -->
-    @vite('resources/css/app.css')
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
     
     <!-- Livewire Styles -->
     @livewireStyles
